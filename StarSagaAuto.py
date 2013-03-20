75be336266a91acc3d0a2cbd08655f561c0d545a
Joshua Buergel <jbuergel@gmail.com>
2013-03-19 22:50:24 -0700

    Updated licenses.

--- StarSagaAuto.py
+++ StarSagaAuto.py
@@ -1,8 +1,20 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-# things I used - pygtalkrobot, virtualbox api (including http://www.virtualbox.org/svn/vbox/trunk/src/VBox/Frontends/VBoxShell/vboxshell.py)
-# requires PIL
+# Copyright (c) 2012 Joshua Buergel <jbuergel@gmail.com>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 from vboxapi import VirtualBoxManager
 from time import sleep, time
